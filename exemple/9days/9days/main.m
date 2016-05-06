e96d0569974d9b890c06dab14321a6309f1bcc7e
dooshik <dooshik9931@gmail.com>
2016-05-06 13:18:02 +0900

    -10일째 수업

--- exemple/9days/9days/main.m
+++ exemple/9days/9days/main.m
@@ -118,23 +118,81 @@ void factorial(int num){
     }
     printf("%d는 factorial은 %d",num, result);
 }
-
+//369 나
 void game(int ans){
-    for (int i = 1; i<29; i++) {
+    for (int i = 1; i<=29; i++) {
         // ans%10/3==0
-        printf("%d / %d = %d\n", ans%10, i, dan*i);
+        if (i%10==3 || i%10==6 || i%10==9) {
+            printf("*\n");
+        }else{
+            printf("%d\n",i );
+        }
+            
+     
         
     }
 }
+
+void hardGame(int ans){
+    for(int i =1; i>=1;  i++){
+        //
+        
+        }
+    for (int j=10; j>=10; j++){
+        
+    }
+           }
+        
+//스왑 함수 만들기
+
+//삼각수 구하기
+
+
+int triangularNum(int Num){
+    int result = 0;
+    for(int i=0; i<=Num; i++ ){
+        result += i;
+        
+    }
+    return result;
+}
+//삼각수 구하기 2
+void triangularNum1(int num,int value){
+    
+    //두수 사이에 5로 나눳을때 나머지가 0인 숫자를 구하고 그걸 더한다
+    //숫자 두개사이의 숫자를 불러 올 수 있는 코드
+
+    for (int i = num; i<=value; i++) {
+        
+        if(i%5==0){
+            int triNum = triangularNum(i);
+            printf("%d\n",triNum);
+        }
+        
+    }
+}
+//각 자리수 더하는 함수
+void addAllNum(int num){
+    for (int i = 1 ; i <= num; i++) {
+        //그 자리의 숫자를 한자리 숫자로 만드는 함수
+        //구한 함수를 더하는 함수
+        
+        
+    }
+}
+
 
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
       
-        factorial(5);
-        
+//        factorial(5);
+//        game(10);
         // insert code here...
+//        triangularNum(10);
+        triangularNum1(6, 25);
         NSLog(@"Hello, World!");
     }
     return 0;
 }
+
