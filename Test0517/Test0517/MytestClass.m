f9066b7df5a8a180f1530178514a63d7e4a571bd
dooshik <dooshik9931@gmail.com>
2016-05-19 00:02:02 +0900

    160518algorismProject

---
+++ Test0517/Test0517/MytestClass.m
@@ -0,0 +1,13 @@
+//
+//  MytestClass.m
+//  Test0517
+//
+//  Created by User on 2016. 5. 17..
+//  Copyright © 2016년 Shin. All rights reserved.
+//
+
+#import "MytestClass.h"
+
+@implementation MytestClass
+
+@end
---
+++ Test0517/Test0517/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Test0517
+//
+//  Created by User on 2016. 5. 17..
+//  Copyright © 2016년 Shin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
